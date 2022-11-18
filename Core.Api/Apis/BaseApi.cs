9a138df5d76d9b7b46a2528bac2c0bdf1ec98deb
Hải Nguyễn <hoanghai03021999@gmail.com>
2022-11-18 22:27:03 +0700

    Add project files.

---
+++ Core.Api/Apis/BaseApi.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Api.Apis
+{
+    [ApiController]
+    [Route("[controller]s")]
+    public abstract class BaseApi : ControllerBase
+    {
+    }
+}
---
+++ Core.Domain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Core.Domain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
