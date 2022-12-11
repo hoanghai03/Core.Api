df1d9153aa6060dbed0bf1057696f297b19aed4e
Hải Nguyễn <hoanghai03021999@gmail.com>
2022-12-11 15:00:28 +0700

    api-gateway

---
+++ Core.Api/Controllers/CartController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Api.Controllers
+{
+    [Route("api/v1/[controller]/[action]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        [HttpGet]
+        public string First()
+        {
+            return "Cart - First";
+        }
+
+        [HttpGet]
+        public string Second()
+        {
+            return "Cart - Second";
+        }
+    }
+}
---
+++ WebAPI.Product/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Product.Controllers
+{
+    [Route("api/v1/[controller]/[action]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public string First()
+        {
+            return "Product - First";
+        }
+
+        [HttpGet]
+        public string Second()
+        {
+            return "Product - Second";
+        }
+    }
+}
